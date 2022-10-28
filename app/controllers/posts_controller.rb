4601f2c35bb237cc2918d3e5c70e857fd2a80923
Berkay Akbas <berkayakbas@gmail.com>
2022-10-28 19:25:41 +0300

    Add views for each action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,14 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @posts = Post.all
+    @user = User.find_by_id(params[:user_id])
+    @users = User.all
+  end
 
-  def show; end
+  def show
+    @post = Post.find_by_id(params[:id])
+    @posts = Post.all
+    @users = User.all
+    @user = User.find_by_id(params[:user_id])
+  end
 end
