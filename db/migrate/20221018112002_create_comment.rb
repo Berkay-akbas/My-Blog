ea85d82fdb7f5f5cae33801b089d80935ec7fbc5
Berkay Akbas <berkayakbas@gmail.com>
2022-10-29 11:34:59 +0300

    Fix migration problem

--- db/migrate/20221018112002_create_comment.rb
+++ db/migrate/20221018112002_create_comment.rb
@@ -1,8 +1,8 @@
 class CreateComment < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.integer :author_id
-      t.integer :post_id
+      t.references :author, foreign_key: { to_table: 'users' }
+      t.references :post, null: false, foreign_key: true
       t.string :text
       t.timestamps
     end
