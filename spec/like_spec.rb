391cb46e29ff515bb73fd4bb8fcbb86a2d66f15c
Berkay Akbas <berkayakbas@gmail.com>
2022-10-31 19:14:42 +0300

    Write tests for ingtegration tests

--- spec/like_spec.rb
+++ spec/like_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  let(:user) { User.create(name: 'John Doe', posts_count: 5, id: 1) }
+  let(:user) { User.create(name: 'John Doe', posts_count: 5, id: 102) }
   let(:post) { Post.create(title: 'title', text: 'text', comments_count: 0, likes_count: 0, author_id: user.id) }
   subject { Like.create(author_id: user.id, post_id: post.id) }
 
