9057c8242aa136a32dc853c4783296d675f84737
Waleed Amjad <waleedamjad99@hotmail.com>
2022-11-02 20:12:02 +0500

    Fix authorization problem

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,5 +1,12 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(user); end
+  def initialize(user)
+  return unless user.present?
+    can :manage, Post, author_id: user.id
+    can :manage, Comment, author_id: user.id
+    can :manage, Like, author_id: user.id
+    can :manage, User, id: user.id
+    can :manage, :all if user.role == 'admin'
+end
 end
