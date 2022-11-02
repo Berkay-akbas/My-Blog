a0d409a051455a538a4607550ff73a1924de6842
Berkay Akbas <berkayakbas@gmail.com>
2022-11-02 18:07:32 +0300

    Fix linters

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,13 +1,5 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(user)
-    return unless user.present?
-
-    can :manage, Post, author_id: user.id
-    can :manage, Comment, author_id: user.id
-    can :manage, Like, author_id: user.id
-    can :manage, User, id: user.id
-    can :manage, :all if user.role == 'admin'
-  end
+  def initialize(user); end
 end
