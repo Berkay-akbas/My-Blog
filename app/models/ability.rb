6c3a4ce3629d5ba8c6210883c77bb155a495ad15
Waleed Amjad <waleedamjad99@hotmail.com>
2022-11-02 20:12:27 +0500

    Fix linter errors

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,11 +2,12 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-  return unless user.present?
+    return unless user.present?
+
     can :manage, Post, author_id: user.id
     can :manage, Comment, author_id: user.id
     can :manage, Like, author_id: user.id
     can :manage, User, id: user.id
     can :manage, :all if user.role == 'admin'
-end
+  end
 end
