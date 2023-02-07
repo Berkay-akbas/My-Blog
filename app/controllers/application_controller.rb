bc976e9febd9e5a9896a7bf69334e78835e34c2f
Berkay Akbas <berkayakbas@gmail.com>
2023-02-07 16:59:06 +0300

    Change default route for live server

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,6 @@ class ApplicationController < ActionController::Base
 
   before_action :update_allowed_parameters, if: :devise_controller?
 
-  def self.default_url_options
-    { host: ENV['HOST'] || 'localhost:3000' }
-  end
-
   def after_sign_in_path_for(_resource)
     users_path
   end
